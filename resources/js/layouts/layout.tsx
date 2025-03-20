d08b71f4c9e7f4e647c77de8db8a88f723049f27
SHRIMPTAILS_\shrim <shrimptail.f3@gmail.com>
2025-03-20 22:01:12 +0900

    #1 サービス層とリポジトリ層とそのテストを作成

--- resources/js/layouts/layout.tsx
+++ resources/js/layouts/layout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react';
 import * as PropTypes from 'prop-types';
 import { Link } from '@inertiajs/react';
+import { AppBar, Toolbar, Typography, Button, Container } from '@mui/material';
 
 interface LayoutProps {
     children: ReactNode;
@@ -8,14 +9,34 @@ interface LayoutProps {
 
 export default function Layout({ children }: LayoutProps) {
     return (
-        <main>
-            <header>
-                <Link href="/">Home</Link>
-                <Link href="/about">About</Link>
-                <Link href="/contact">Contact</Link>
-            </header>
-            <article>{children}</article>
-        </main>
+        <>
+            {/* ヘッダー */}
+            <AppBar position="static" color="primary">
+                <Container>
+                    <Toolbar>
+                        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+                            MyApp
+                        </Typography>
+                        <Button color="inherit" component={Link} href="/">
+                            Home
+                        </Button>
+                        <Button color="inherit" component={Link} href="/about">
+                            About
+                        </Button>
+                        <Button
+                            color="inherit"
+                            component={Link}
+                            href="/contact"
+                        >
+                            Contact
+                        </Button>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+
+            {/* コンテンツ */}
+            <Container sx={{ mt: 4 }}>{children}</Container>
+        </>
     );
 }
 
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index']);
+Route::get('/', [UserController::class, 'index'])->name('welcomePage');
